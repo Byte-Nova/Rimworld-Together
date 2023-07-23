09a7f309af8a694ecd3a747be8dc7488fcf77a46
Nova-Atomic <vulnerableoperatingsystems@gmail.com>
2023-07-23 11:25:19 +0200

    Changed serializers to being separated for server and client again

---
+++ Source/Client/Misc/Serializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RimworldTogether
+{
+    public static class Serializer
+    {
+        public static string SerializeToString(object serializable)
+        {
+            return JsonUtility.ToJson(serializable);
+        }
+
+        public static T SerializeFromString<T>(string serializable)
+        {
+            return JsonUtility.FromJson<T>(serializable);
+        }
+
+        public static Packet SerializeToPacket(string serializable)
+        {
+            return JsonUtility.FromJson<Packet>(serializable);
+        }
+
+        public static void SerializeToFile(string path, object serializable)
+        {
+            File.WriteAllText(path, JsonUtility.ToJson(serializable, true));
+        }
+
+        public static T SerializeFromFile<T>(string path)
+        {
+            return JsonUtility.FromJson<T>(File.ReadAllText(path));
+        }
+    }
+}
