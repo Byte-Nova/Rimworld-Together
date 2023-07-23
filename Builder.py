da81a8ab7aa749e161e3936451abf0fb150d0c6c
emelrad12 <emelrad12@gmail.com>
2023-07-23 12:53:48 +0200

    Add build process.

---
+++ Builder.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import subprocess
+
+modSteamId = "3005289691"  # Replace with your actual Steam ID
+
+source_dir = "ModData"
+destination_dir = f"Build/{modSteamId}"
+source_solution = "Source/Client/GameClient.csproj"
+if os.path.exists(destination_dir):
+    shutil.rmtree(destination_dir)
+# Copy files from source_dir to destination_dir
+shutil.copytree(source_dir, destination_dir)
+
+# Build the C# project
+subprocess.run(["dotnet", "build", source_solution, "--configuration", "Release"])
+
+# Copy the output dll to dll_destination
+dll_output_path = "Source/Client/bin/Release/GameClient.dll"  # Adjust the path based on your configuration
+shared_dll_output_path = "Source/Client/bin/Release/Shared.dll"  # Adjust the path based on your configuration
+dll_destination_path = os.path.join(destination_dir, "Current/Assemblies/RimworldTogether.dll")
+shared_dll_destination_path = os.path.join(destination_dir, "Current/Assemblies/Shared.dll")
+
+# If the destination path doesn't exist, create it
+os.makedirs(os.path.dirname(dll_destination_path), exist_ok=True)
+
+# Copy and rename the dll file
+shutil.copy2(dll_output_path, dll_destination_path)
+shutil.copy2(shared_dll_output_path, shared_dll_destination_path)
+mod_paths = [
+    "C:/Games/Rimworld/Mods",
+    "C:/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods"
+]
+
+# Copy mod to each path if it exists
+for mod_path in mod_paths:
+    if os.path.exists(mod_path):
+        mod_specific_path = os.path.join(mod_path, modSteamId)
+
+        # Clear the specific mod directory if it already exists
+        if os.path.exists(mod_specific_path):
+            shutil.rmtree(mod_specific_path)
+
+        # Copy mod to mod_path
+        shutil.copytree(destination_dir, mod_specific_path)
+
