a218123a62dd2aa4e82ee134f68af846f8df575c
emelrad12 <emelrad12@gmail.com>
2023-07-26 13:26:55 +0200

    Remove old folder

---
+++ Source/SharedData/Network/Communicators/HostClientCommunicator.cs
@@ -0,0 +1,21 @@
+namespace RimworldTogether.Shared.Network
+{
+    // One host to many clients
+    public class HostClientCommunicator
+    {
+        public void HostSend()
+        {
+            
+        }
+
+        public void ServerReceive()
+        {
+            
+        }
+
+        public void ClientSend()
+        {
+            
+        }
+    }
+}
