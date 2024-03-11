2ccdf03d37253889089ee49afcdd40d41917130b
Nova <159603018+Byte-Nova@users.noreply.github.com>
2024-03-11 22:38:55 +0100

    Revert towards older development version

--- Source/Client/Network/Network.cs
+++ Source/Client/Network/Network.cs
@@ -32,7 +32,7 @@ namespace GameClient
                 Threader.GenerateThread(Threader.Mode.Health);
                 Threader.GenerateThread(Threader.Mode.KASender);
 
-                Log.Message($"[Rimworld Together] > Connected to server");
+                Logs.Message($"[Rimworld Together] > Connected to server");
             }
 
             else
