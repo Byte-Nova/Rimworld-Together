982502060906960fca0bb3ab4c2b11b21470c87c
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2024-08-26 22:50:24 +0200

    Chat and Server commands cleaning

---
+++ Source/Server/Misc/Commands/ServerCommand.cs
@@ -0,0 +1,18 @@
+public class ServerCommand
+{
+    public string prefix;
+
+    public string description;
+
+    public int parameters;
+
+    public Action commandAction;
+
+    public ServerCommand(string prefix, int parameters, string description, Action commandAction)
+    {
+        this.prefix = prefix;
+        this.parameters = parameters;
+        this.description = description;
+        this.commandAction = commandAction;
+    }
+}
