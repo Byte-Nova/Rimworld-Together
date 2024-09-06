26a64b6b66a5fba28e4b00432ccafdef5f11b6fa
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2024-09-06 18:30:04 +0200

    Update LoadAllMods.cs

--- Source/Client/Core/LoadAllMods.cs
+++ Source/Client/Core/LoadAllMods.cs
@@ -10,26 +10,32 @@ namespace GameClient
     public static class LoadAllMods 
     {
         public static readonly string assemblyPath = Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString(), "Patches");
+
         public static readonly string fileExtension = ".dll";
+
         public static void LoadAllPatchAssemblies()
         {
             Dictionary<string,string> allModsToLoad = GetAllPatchedMods();
             foreach(string name in allModsToLoad.Keys)
             {
-                if(LoadedModManager.RunningModsListForReading.Any(mod => mod.Name == allModsToLoad[name]))
+                if (LoadedModManager.RunningModsListForReading.Any(mod => mod.Name == allModsToLoad[name]))
                 {
-                    if(File.Exists(Path.Combine(assemblyPath, name + fileExtension))) {
+                    if (File.Exists(Path.Combine(assemblyPath, name + fileExtension))) 
+                    {
                         Assembly assembly = Assembly.LoadFrom(Path.Combine(assemblyPath, name + fileExtension));
                         Master.loadedPatches.Add(name,assembly);
+                        
                         try 
                         {
                             Type toUse = typeof(LoadAllMods);
                             MethodInfo methodInfo = toUse.GetMethod(name, BindingFlags.NonPublic | BindingFlags.Static);
                             methodInfo.Invoke(name,null);
+
                             Logger.Message($"Loaded patch {name} patching {allModsToLoad[name]}");
                         }
-                        catch (Exception ex){ Logger.Warning($"Failed to load patch {name} because :\n{ex.ToString()}"); }
+                        catch (Exception ex){ Logger.Error($"Failed to load patch {name} because :\n{ex.ToString()}"); }
                     }
+                    else Logger.Error($"Couldn't find patch for mod {allModsToLoad[name]}");
                 }
             }
         } 
