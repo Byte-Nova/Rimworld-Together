a615831fac0839982fa4e4e90b154c0a7bab1368
Byte-Nova <159603018+Byte-Nova@users.noreply.github.com>
2024-10-06 01:38:04 +0200

    Update ModStuff.cs

--- Source/Client/Core/Configs/ModStuff.cs
+++ Source/Client/Core/Configs/ModStuff.cs
@@ -47,7 +47,7 @@ namespace GameClient
 
             listingStandard.GapLine();
             listingStandard.Label("External Sources");
-            if (listingStandard.ButtonTextLabeled("Check out the mod's wiki!", "Open")) StartProcess("https://rimworldtogether.github.io/Guide");
+            if (listingStandard.ButtonTextLabeled("Check out the mod's wiki!", "Open")) StartProcess("https://github.com/RimworldTogether/Rimworld-Together/wiki");
             if (listingStandard.ButtonTextLabeled("Check out the mod's Github!", "Open")) StartProcess("https://github.com/RimworldTogether/Rimworld-Together");
             if (listingStandard.ButtonTextLabeled("Check out the mod's incompatibility list!", "Open")) StartProcess("https://github.com/RimworldTogether/Rimworld-Together/blob/development/IncompatibilityList.md");
             if (listingStandard.ButtonTextLabeled("Join the mod's Discord community!", "Open")) StartProcess("https://discord.gg/yUF2ec8Vt8");
