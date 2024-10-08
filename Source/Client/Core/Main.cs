b165a7abd388da4a4601f5d1444d2b7f7954e9c7
Erag0n001 <148992045+Erag0n001@users.noreply.github.com>
2024-10-08 14:11:37 -0400

    Fixed prepatcher

--- Source/Client/Core/Main.cs
+++ Source/Client/Core/Main.cs
@@ -29,9 +29,7 @@ namespace GameClient
                 CaravanManagerHelper.SetCaravanDefs();
                 PreferenceManager.LoadClientPreferences();
 
-                //FIXME
-                //Causes issues to load sometimes
-                //CompatibilityManager.LoadAllPatchedAssemblies();
+                CompatibilityManager.LoadAllPatchedAssemblies();
             }
         }
 
@@ -54,21 +52,20 @@ namespace GameClient
             Master.mainPath = GenFilePaths.SaveDataFolderPath;
             Master.modFolderPath = Path.Combine(Master.mainPath, "RimWorld Together");
 
+            Master.modAssemblyPath = Path.Combine(LoadedModManager.GetMod<Mod>().Content.ModMetaData.RootDir.FullName, "Current", "Assemblies");
+            Master.compatibilityPatchesFolderPath = Path.Combine(Master.modAssemblyPath, "Patches");
+
             Master.connectionDataPath = Path.Combine(Master.modFolderPath, "ConnectionData.json");
             Master.clientPreferencesPath = Path.Combine(Master.modFolderPath, "Preferences.json");
             Master.loginDataPath = Path.Combine(Master.modFolderPath, "LoginData.json");
             Master.savesFolderPath = GenFilePaths.SavedGamesFolderPath;
 
-            //FIXME
-            //Causes issues to load sometimes            
-            // Master.modAssemblyPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString();
-            // Master.compatibilityPatchesFolderPath = Path.Combine(Master.modAssemblyPath, "Patches");
+            Master.modAssemblyPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString();
+            Master.compatibilityPatchesFolderPath = Path.Combine(Master.modAssemblyPath, "Patches");
 
             if (!Directory.Exists(Master.modFolderPath)) Directory.CreateDirectory(Master.modFolderPath);
 
-            //FIXME
-            //Causes issues to load sometimes
-            // if (!Directory.Exists(Master.compatibilityPatchesFolderPath)) Directory.CreateDirectory(Master.compatibilityPatchesFolderPath);
+            if (!Directory.Exists(Master.compatibilityPatchesFolderPath)) Directory.CreateDirectory(Master.compatibilityPatchesFolderPath);
         }
 
         public static void CreateUnityDispatcher()
@@ -81,6 +78,14 @@ namespace GameClient
 
                 Logger.Message($"Created dispatcher for version {CommonValues.executableVersion}");
             }
+        }
+
+
+        public class Mod : Verse.Mod // This is used to get the mod folder, safe to ignore or throw in another file or class.
+        {
+            public Mod(ModContentPack content) : base(content){}
+
+            public override void DoSettingsWindowContents(Rect inRect){base.DoSettingsWindowContents(inRect);}
         }
     }
 }
