e79469447ed0a5956a0d0e397485b5c8dda19384
SimobiSirOP <simobisirop@gmail.com>
2024-10-18 21:26:54 +0300

    Adding major threats to force hardcore List. (Also, with future compat)

--- Source/Client/Patches/HardmodePatches.cs
+++ Source/Client/Patches/HardmodePatches.cs
@@ -1,11 +1,16 @@
-﻿using HarmonyLib;
-using Shared;
+﻿using System;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Verse;
 
 namespace GameClient
 {
     public class HardmodePatches
     {
+        public static List<string> forceSavedThreats = new List<string>() {"ThreatBig"};
+
         [HarmonyPatch(typeof(Pawn), nameof(Pawn.Kill))]
         public static class Pawn_Kill_Patches
         {
@@ -14,6 +19,18 @@ namespace GameClient
             {
                 if (!SessionValues.actionValues.HardcoreMode) return;
                 if (__instance.Faction != null && __instance.Faction.IsPlayer)
+                    SaveManager.ForceSave();
+            }
+        }
+
+        [HarmonyPatch(typeof(LetterStack), nameof(LetterStack.ReceiveLetter), new[] {typeof(Letter), typeof(string), typeof(int), typeof(bool)})]
+        public static class Threat_Patches
+        {
+            [HarmonyPostfix]
+            public static void DoPost(Letter let)
+            {
+                if (!SessionValues.actionValues.HardcoreMode) return;
+                if (forceSavedThreats.Contains(let.def.defName))
                     SaveManager.ForceSave();
             }
         }
