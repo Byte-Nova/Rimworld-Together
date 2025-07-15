d79f304efda6f8c08095ef58278979845cc44227
Simon Uhrmann <suhrmann@posteo.de>
2025-07-15 21:36:23 +0200

    Set "25555" as default value for port input in `ConnectionManager`.

--- Source/Client/Managers/ConnectionManager.cs
+++ Source/Client/Managers/ConnectionManager.cs
@@ -20,8 +20,13 @@ namespace GameClient.Managers
 
         public static void ShowConnectDialogs()
         {
-            RT_Dialog_Base.PushNewDialog(new RT_Dialog_Inputs("Connection Details", new string[] { "IP", "Port" }, new bool[] { false, false },
-                delegate { ParseConnectionDetails(); }));
+            var dialogConnDetails = new RT_Dialog_Inputs("Connection Details",
+                new string[] { "IP", "Port" },
+                new bool[] { false, false },
+                delegate { ParseConnectionDetails(); },
+                defaultValues: new string[] { "", "25555" }
+            );
+            RT_Dialog_Base.PushNewDialog(dialogConnDetails);
         }
 
         public static void ParseConnectionDetails()
