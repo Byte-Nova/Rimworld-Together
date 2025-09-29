102d43cb90c8e20ef129b491aceb1a785402b160
jRivers <juho.velimatti.joensuu@gmail.com>
2025-09-29 21:55:46 +0300

    Proper check for whitespace

--- Source/Shared/Misc/StringChecker.cs
+++ Source/Shared/Misc/StringChecker.cs
@@ -9,13 +9,13 @@ namespace Shared
         {
             "<", ">", ":", "\"", "/", "|", "?", "*", "CON", "PRN", "AUX", "NUL", "COM1",
             "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7",
-            "LPT8", "LPT9", " "
+            "LPT8", "LPT9"
         };
 
         public static bool CheckIfStringValid(string toCheck)
         {
             if (string.IsNullOrWhiteSpace(toCheck)) return false;
-            if (IllegalSequences.Any(s => toCheck.IndexOf(s, StringComparison.InvariantCultureIgnoreCase) > -1)) return false;
+            if (IllegalSequences.Any(s => toCheck.IndexOf(s, StringComparison.InvariantCultureIgnoreCase) > -1 || s.Any(char.IsWhiteSpace))) return false;
 
             return true;
         }
